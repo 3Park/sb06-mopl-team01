8d3d87b26f4e37bc19bd52fcb73fa127e2cffb13
김재원 <dumdum7@hs.ac.kr>
2026-01-27 15:38:35 +0900

    feat: CursorRequestReviewDto 생성

--- src/main/java/org/example/mopl/content/entity/controller/ReviewController.java
+++ src/main/java/org/example/mopl/content/entity/controller/ReviewController.java
@@ -1,5 +1,19 @@
 package org.example.mopl.content.entity.controller;
 
+import jakarta.validation.Valid;
+import org.example.mopl.content.entity.dto.response.CursorResponseReviewDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/reviews")
 public class ReviewController {
+
+  @GetMapping
+  public ResponseEntity<CursorResponseReviewDto> getAllReviewsByCursor(
+  ) {
+    return ResponseEntity.ok().build();
+  }
 
 }
