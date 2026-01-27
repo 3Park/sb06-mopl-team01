b04e9b04f3301c69090b643f96b29b892cf5a500
김재원 <dumdum7@hs.ac.kr>
2026-01-27 14:06:11 +0900

    feat: Playlist 엔티티 추가

--- src/main/java/org/example/mopl/contentevaluation/entity/Playlist.java
+++ src/main/java/org/example/mopl/contentevaluation/entity/Playlist.java
@@ -1,4 +1,48 @@
 package org.example.mopl.contentevaluation.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.example.mopl.user.entity.User;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Getter
+@Entity
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "playlists")
 public class Playlist {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  @UuidGenerator
+  @Column(name = "uuid", nullable = false, unique = true)
+  private UUID uuid;
+
+  @JoinColumn(name = "user_id", nullable = false)
+  @ManyToOne(optional = false)
+  private User user;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false)
+  private Instant createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private Instant updatedAt;
+
 }
