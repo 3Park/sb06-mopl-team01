a55b63a2867e40daeab22aff0eb2505a5bf06d69
김재원 <dumdum7@hs.ac.kr>
2026-01-27 13:38:30 +0900

    feat: Content 엔티티 생성

--- src/main/java/org/example/mopl/content/entity/Content.java
+++ src/main/java/org/example/mopl/content/entity/Content.java
@@ -1,4 +1,50 @@
 package org.example.mopl.content.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Getter
+@Entity
+@Table(name = "contents")
 public class Content {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  @Column(name = "type", nullable = false)
+  private String type;
+
+  @Column(name = "title", nullable = false)
+  private String title;
+
+  @Column(name = "description", nullable = false)
+  private String description;
+
+  @Column(name = "thumbnail_url", nullable = false)
+  private String thumbnailUrl;
+
+  @Column(name = "uuid", nullable = false, unique = true)
+  private UUID uuid;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false)
+  private Instant createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private Instant updatedAt;
+
+
+
 }
