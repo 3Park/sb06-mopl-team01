0287e4e6f3f86568186445c5919984cd40634eba
김재원 <dumdum7@hs.ac.kr>
2026-01-27 14:34:28 +0900

    feat: Review 엔티티 추가

---
+++ src/main/java/org/example/mopl/content/entity/Review.java
@@ -0,0 +1,53 @@
+package org.example.mopl.content.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.example.mopl.user.entity.User;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Getter
+@Entity
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "reviews")
+public class Review {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  @UuidGenerator
+  @Column(name = "uuid", nullable = false, unique = true)
+  private UUID uuid;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "content_id", nullable = false)
+  private Content content;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false)
+  private Instant createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private Instant updatedAt;
+
+}
