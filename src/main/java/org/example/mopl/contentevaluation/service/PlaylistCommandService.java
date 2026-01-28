853b164617e075bfc70867c19a64ceb92756739f
김재원 <dumdum7@hs.ac.kr>
2026-01-28 16:15:29 +0900

    feat: 의존관계 주입

--- src/main/java/org/example/mopl/contentevaluation/service/PlaylistCommandService.java
+++ src/main/java/org/example/mopl/contentevaluation/service/PlaylistCommandService.java
@@ -1,5 +1,15 @@
 package org.example.mopl.contentevaluation.service;
 
+import lombok.RequiredArgsConstructor;
+import org.example.mopl.contentevaluation.repository.PlaylistCommandRepository;
+import org.example.mopl.contentevaluation.repository.PlaylistQueryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class PlaylistCommandService {
+
+  private final PlaylistCommandRepository playlistCommandRepository;
+  private final PlaylistQueryRepository playlistQueryRepository;
 
 }
