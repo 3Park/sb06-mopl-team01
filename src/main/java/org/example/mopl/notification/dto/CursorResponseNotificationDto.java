07376a786ab27ac11e343e4f0a89b53d04e392d1
전채연 <ami02074657@gmail.com>
2026-01-30 00:59:07 +0900

    [Feat] NotificationDto, CursorResponseNotificationDto 및 Enums 생성

---
+++ src/main/java/org/example/mopl/notification/dto/CursorResponseNotificationDto.java
@@ -0,0 +1,20 @@
+package org.example.mopl.notification.dto;
+
+import lombok.Builder;
+import org.example.mopl.notification.enums.SortBy;
+import org.hibernate.query.SortDirection;
+
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+public record CursorResponseNotificationDto(
+        List<NotificationDto> data,
+        String nextCursor,
+        UUID nextIdAfter,
+        boolean hasNext,
+        Long totalCount,
+        SortBy sortBy,
+        SortDirection sortDirection
+) {
+}
