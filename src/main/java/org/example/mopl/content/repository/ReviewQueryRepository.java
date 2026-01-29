ecacec0ad31fbe160ada161ce30aaf8f584bd09f
김재원 <dumdum7@hs.ac.kr>
2026-01-29 11:14:46 +0900

    feat: findAllByCursor 추가

--- src/main/java/org/example/mopl/content/repository/ReviewQueryRepository.java
+++ src/main/java/org/example/mopl/content/repository/ReviewQueryRepository.java
@@ -3,6 +3,7 @@ package org.example.mopl.content.repository;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -40,6 +41,28 @@ public class ReviewQueryRepository {
   @Transactional(readOnly = true)
   public Page<ReviewDto> findAllByCursor(CursorRequestReviewDto request) {
 
+    List<Review> reviewList = queryFactory.selectFrom(QReview.review)
+        .join(QReview.review.content, QContent.content)
+        .fetchJoin()
+        // Todo : User join 추가
+        .where(buildDynamicQueryByCursor(request))
+        .orderBy(
+            request.sortDirection().equals("DESCENDING") ?
+                (request.sortBy().equals("rating") ?
+                    QReview.review.rating.desc() :
+                    QReview.review.createdAt.desc()
+                ) :
+                (request.sortBy().equals("rating") ?
+                    QReview.review.rating.asc() :
+                    QReview.review.createdAt.asc()
+                ),
+            request.sortDirection().equals("DESCENDING") ?
+                QReview.review.uuid.desc() :
+                QReview.review.uuid.asc()
+        )
+        .limit(request.limit() + 1)
+        .fetch();
+
     return null;
 
   }
@@ -51,42 +74,57 @@ public class ReviewQueryRepository {
     // 콘텐츠 id
     builder.and(QReview.review.content.uuid.eq(request.contentId()));
 
-    // 정렬 방향 & 정렬 기준
+    // 커서 : createdAt, rating
+    // 보조 커서 : uuid
+    if (request.sortDirection().equals("DESCENDING")) {
 
-    if (request.sortBy().equals("rating")) {
-
-      if (request.sortDirection().equals("DESCENDING")) {
-        builder.and(QReview.review.rating.lt(Double.parseDouble(request.cursor())));
-      } else {
-        builder.and(QReview.review.rating.gt(Double.parseDouble(request.cursor())));
+      switch (request.sortBy()) {
+        case "rating":
+          if (request.cursor() != null) {
+            builder.and(QReview.review.rating.lt(Double.parseDouble(request.cursor())));
+          }
+          if (request.idAfter() != null) {
+            builder.and(QReview.review.uuid.lt(request.idAfter()));
+          }
+          break;
+        case "createdAt":
+          if (request.cursor() != null) {
+            builder.and(QReview.review.createdAt.lt(Instant.parse(request.cursor())));
+          }
+          if (request.idAfter() != null) {
+            builder.and(QReview.review.uuid.lt(request.idAfter()));
+          }
+          break;
+        default:
+          throw new IllegalArgumentException("Invalid sortBy field: " + request.sortBy());
       }
 
     } else {
 
-      if (request.sortDirection().equals("DESCENDING")) {
-        builder.and(QReview.review.createdAt.lt(Instant.parse(request.cursor())));
-      } else {
-        builder.and(QReview.review.createdAt.gt(Instant.parse(request.cursor())));
-      }
-
-    }
-
-    // 커서
-
-    // 보조 커서 UUID
-    if (request.idAfter() != null) {
-
-      if (request.sortDirection().equals("DESCENDING")) {
-        builder.and(QContent.content.uuid.lt(request.idAfter()));
-      } else {
-        builder.and(QContent.content.uuid.gt(request.idAfter()));
+      switch (request.sortBy()) {
+        case "rating":
+          if (request.cursor() != null) {
+            builder.and(QReview.review.rating.gt(Double.parseDouble(request.cursor())));
+          }
+          if (request.idAfter() != null) {
+            builder.and(QReview.review.uuid.gt(request.idAfter()));
+          }
+          break;
+        case "createdAt":
+          if (request.cursor() != null) {
+            builder.and(QReview.review.createdAt.gt(Instant.parse(request.cursor())));
+          }
+          if (request.idAfter() != null) {
+            builder.and(QReview.review.uuid.gt(request.idAfter()));
+          }
+          break;
+        default:
+          throw new IllegalArgumentException("Invalid sortBy field: " + request.sortBy());
       }
 
     }
 
     return builder;
 
-
   }
-
 }
