39ba6f127aabe49eb3ece45b45549da2c3630110
김재원 <dumdum7@hs.ac.kr>
2026-01-29 10:29:35 +0900

    feat: Subscribe Repository 추가

---
+++ src/main/java/org/example/mopl/contentevaluation/repository/SubscribeCommandRepository.java
@@ -0,0 +1,8 @@
+package org.example.mopl.contentevaluation.repository;
+
+import org.example.mopl.contentevaluation.entity.Subscribe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscribeCommandRepository extends JpaRepository<Subscribe, Long> {
+
+}
---
+++ src/main/java/org/example/mopl/contentevaluation/repository/SubscribeQueryRepository.java
@@ -0,0 +1,5 @@
+package org.example.mopl.contentevaluation.repository;
+
+public class SubscribeQueryRepository {
+
+}
