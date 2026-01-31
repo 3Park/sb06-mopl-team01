e2b503a30c92e4b84cde455de7702d3bf6c1d821
김재원 <dumdum7@hs.ac.kr>
2026-01-31 16:38:51 +0900

    feat: SportCrawlerClient 인터페이스 생성

---
+++ src/main/java/org/example/mopl/content/crawler/SportCrawlerClient.java
@@ -0,0 +1,12 @@
+package org.example.mopl.content.crawler;
+
+import java.util.List;
+import org.example.mopl.content.dto.ContentFetchResultDto;
+
+public interface SportCrawlerClient {
+
+  List<String> fetchLeagues();
+
+  List<ContentFetchResultDto> fetchRecentMatches(String league);
+
+}
