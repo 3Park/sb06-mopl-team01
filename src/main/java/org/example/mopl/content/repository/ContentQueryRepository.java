2944a094d1cf97d13c076f092c22a16b60e430fb
김재원 <dumdum7@hs.ac.kr>
2026-02-02 13:37:16 +0900

    feat: 태그 검색 조건 추가

--- src/main/java/org/example/mopl/content/repository/ContentQueryRepository.java
+++ src/main/java/org/example/mopl/content/repository/ContentQueryRepository.java
@@ -170,6 +170,26 @@ public class ContentQueryRepository {
     // 검색 키워드
     builder.and(QContent.content.title.containsIgnoreCase(request.keywordLike()));
 
+    if (!request.tagsIn().isEmpty()) {
+
+      List<Long> contentIdsWithAllTags = queryFactory
+          .select(QContentTag.contentTag.content.id)
+          .from(QContentTag.contentTag)
+          .join(QContentTag.contentTag.tag, QTag.tag)
+          .where(QTag.tag.name.in(request.tagsIn()))
+          .groupBy(QContentTag.contentTag.content.id)
+          .having(QContentTag.contentTag.content.id.count().eq((long) request.tagsIn().size()))
+          .fetch();
+
+      if (!contentIdsWithAllTags.isEmpty()) {
+        builder.and(QContent.content.id.in(contentIdsWithAllTags));
+      } else {
+        // 조건에 맞는 콘텐츠가 없을 경우 빈 결과를 반환하기 위해 항상 거짓인 조건 추가
+        builder.and(QContent.content.id.eq(-1L));
+      }
+
+    }
+
     // 커서 : createdAt, watcherCount, rate
     // 보조 커서 : uuid
     if (request.sortDirection().equals("DESCENDING")) {
