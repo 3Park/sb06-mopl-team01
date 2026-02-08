5d669f6a3d86d18cbb98a9c17b816af6894a97f1
전채연 <ami02074657@gmail.com>
2026-02-09 05:57:21 +0900

    [Fix] principal 인증 정보 @AuthenticationPrincipal 통해 받도록 수정

--- src/main/java/org/example/mopl/directmessage/controller/DirectMessageSocketController.java
+++ src/main/java/org/example/mopl/directmessage/controller/DirectMessageSocketController.java
@@ -3,11 +3,14 @@ package org.example.mopl.directmessage.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.mopl.auth.CustomUserDetails;
 import org.example.mopl.directmessage.dto.request.DirectMessageCreateRequest;
 import org.example.mopl.directmessage.service.DirectMessageService;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
@@ -24,11 +27,11 @@ public class DirectMessageSocketController {
     public void sendDirectMessages(
             @DestinationVariable("conversationId") UUID conversationUuid,
             @Payload @Valid DirectMessageCreateRequest request,
-            Principal principal) {
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
         log.info("directmessage 전송, conversationId={}", conversationUuid);
-        if (principal == null) { log.error("인증 정보가 유실되었습니다."); return;}
+        if (userDetails == null) { log.error("인증 정보가 유실되었습니다."); return;}
 
-        UUID senderUuid = UUID.fromString(principal.getName());
+        UUID senderUuid = userDetails.getUserDto().getId();
         directMessageService.saveAndSendMessage(conversationUuid, senderUuid, request.content());
     }
 }
