09b5e373f0ac95f781ac319721356fcd3d8356f4
jihoon-68 <pkrwlgns64@gmail.com>
2026-02-21 01:24:07 +0900

    [JZAQ-160, JZAQ-171] refactor :  코드 리팩토링 및 정리, 시청중인 콘텐츠 이동 불가 수정

--- src/main/java/org/example/mopl/watchtogether/dto/WatchingSessionDto.java
+++ src/main/java/org/example/mopl/watchtogether/dto/WatchingSessionDto.java
@@ -1,6 +1,7 @@
 package org.example.mopl.watchtogether.dto;
 
 import lombok.Builder;
+import org.example.mopl.content.dto.response.ContentDto;
 import org.example.mopl.content.entity.Content;
 import org.example.mopl.watchtogether.model.Watcher;
 import org.example.mopl.watchtogether.model.WatchingSession;
@@ -13,9 +14,9 @@ public record WatchingSessionDto(
         String id,
         Instant createdAt,
         Watcher watcher,
-        Content content
+        ContentDto content
 ) {
-    public WatchingSessionDto(WatchingSession watchingSession, Content content){
+    public WatchingSessionDto(WatchingSession watchingSession, ContentDto content){
         this(
                 watchingSession.getId(),
                 watchingSession.getCreatedAt(),
