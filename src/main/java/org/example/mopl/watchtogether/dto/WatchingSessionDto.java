2bc44857cc729596950dab72d571efa94549d4de
jihoon-68 <pkrwlgns64@gmail.com>
2026-02-24 18:32:47 +0900

    [JZAQ-160] style : 코드 리팩토링 및 정리

--- src/main/java/org/example/mopl/watchtogether/dto/WatchingSessionDto.java
+++ src/main/java/org/example/mopl/watchtogether/dto/WatchingSessionDto.java
@@ -2,12 +2,10 @@ package org.example.mopl.watchtogether.dto;
 
 import lombok.Builder;
 import org.example.mopl.content.dto.response.ContentDto;
-import org.example.mopl.content.entity.Content;
 import org.example.mopl.watchtogether.model.Watcher;
 import org.example.mopl.watchtogether.model.WatchingSession;
 
 import java.time.Instant;
-import java.util.UUID;
 
 @Builder
 public record WatchingSessionDto(
